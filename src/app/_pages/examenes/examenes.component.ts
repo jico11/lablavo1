f0b40fdb0afab12aa1fb03c335fdcfe1a0fe6f29
jico11 <jico.cs@gmail.com>
2017-08-21 18:55:53 -0300

    com

--- src/app/_pages/examenes/examenes.component.ts
+++ src/app/_pages/examenes/examenes.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./examenes.component.scss']
 })
 export class ExamenesComponent implements OnInit {
-
+  texto = 'Seleccione en el apartado izquierdo para ver en detalle los exámenes que se realizan en nuestro laboratorio. ';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getSerologico(){
+    this.texto = 'En el ambito serológico se realizan los siguientes examenes.';
+
   }
 
 }
--- src/app/_pages/examenes/serologicos/serologicos.component.ts
+++ src/app/_pages/examenes/serologicos/serologicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-serologicos',
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./serologicos.component.scss']
 })
 export class SerologicosComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
