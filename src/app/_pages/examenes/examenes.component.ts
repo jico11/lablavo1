5c87b16aeb0e1bb91a2a2015fcf9e6fe5ab7bd42
jico11 <jico.cs@gmail.com>
2017-08-21 22:21:41 -0300

    cambios menores a examenes

--- src/app/_pages/examenes/examenes.component.ts
+++ src/app/_pages/examenes/examenes.component.ts
@@ -12,9 +12,29 @@ export class ExamenesComponent implements OnInit {
   ngOnInit() {
   }
 
+  getExamenes(){
+    this.texto = 'Seleccione en el apartado izquierdo para ver en detalle los exámenes que se realizan en nuestro laboratorio. ';
+  }
   getSerologico(){
-    this.texto = 'En el ambito serológico se realizan los siguientes examenes.';
-
+    this.texto = 'En el ambito Serológico se realizan los siguientes examenes.';
+  }
+  getBioquimicos(){
+    this.texto = 'En el ambito Bioquímico se realizan los siguientes examenes.';
+  }
+  getCultivos(){
+    this.texto = 'En el ambito de los Cultivos se realizan los siguientes examenes.';
+  }
+  getDeposicion(){
+    this.texto = 'En el ambito de Deposicion se realizan los siguientes examenes.';
+  }
+  getHematologicos(){
+    this.texto = 'En el ambito Hematológico se realizan los siguientes examenes.';
+  }
+  getHormonas(){
+    this.texto = 'En el ambito de Hormonas se realizan los siguientes examenes.';
+  }
+  getOrina(){
+    this.texto = 'En el ambito de Orinas se realizan los siguientes examenes.';
   }
 
 }
